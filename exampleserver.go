dc32462d9a581bba2480430cc7aff2fbdac4ef28
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-01-31 09:57:47 +0100

    Added templates and served /static to localhost:666/static

--- exampleserver.go
+++ exampleserver.go
@@ -1,15 +1,79 @@
 package main
 
 import (
+        "fmt"
         "net/http"
-        "./gocomponents"
+        "os"
+        "bufio"
+        "mime"
+        "path/filepath"
+        "log"
+        "html/template"
 )
 
+func static(w http.ResponseWriter, r *http.Request) {
+        // Get path and add gocomponents to it to get the correct file
+        path := "gocomponents/" + r.URL.Path
+
+        // Open file
+        data, err := os.Open(path)
+
+        // If an error occurred, render an error page
+        if err != nil {
+                fmt.Fprintf(w, err.Error())
+                return
+        }
+
+        // Close data on next return
+        defer data.Close()
+
+        // Create a scanner
+        scanner := bufio.NewScanner(data)
+
+        var fileContent string
+
+        // Scan file and save result in fileContent
+        for scanner.Scan() {
+                fileContent += scanner.Text() + "\n"
+        }
+
+        // If the scanner ran into an error, display it
+        if err := scanner.Err(); err != nil {
+                fmt.Fprintf(w, err.Error())
+                return
+        }
+
+        // Get mime type by extension
+        mimeType := mime.TypeByExtension(filepath.Ext(path))
+
+        // Set content type 
+        w.Header().Set("Content-Type", mimeType)
+
+        // Show file
+        fmt.Fprintf(w, fileContent)
+        return;
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-        gocomponents.Header(w, r)
+        // Parse required files
+        t, err := template.ParseFiles("./templates/index.html", "./gocomponents/templates/header.html")
+        
+        // Make sure there are no errors
+        if err != nil  {
+                log.Fatal(err)
+        }
+        
+        // Data for the template
+        templateData := map[string]string{
+                "SiteTitle": "Eggs Benedict",
+        }
+
+        // Execute templates
+        t.ExecuteTemplate(w, "layout", templateData)
 }
 
 func main() {
+        http.HandleFunc("/static/", static)
         http.HandleFunc("/", index)
         http.ListenAndServe(":666", nil)
 }
